cdc324800d8f3b41388593278d8c3985e94d07df
WWcode Recife <karina@womenwhocode.com>
2016-10-05 17:30:06 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,62 @@
+/*****************************/
+
+        //objetos.js - Meetup 2
+
+        // Women Who Code Recife
+
+ /*****************************/
+
+
+ // Obs: Lembrando teste o seu código no console do navegador.
+
+
+
+
+
+/***********  # 1 Criar Objeto Notebook.*************/
+
+// código aqui
+
+
+
+
+
+
+/*********** # 2 Adicionando as propriedades. *************/
+
+// código aqui
+
+
+
+
+
+
+/***********  # Acessando objeto com a notaçāo "." *************/
+
+
+// código aqui
+
+
+
+
+
+/***********  Acessando objeto com a notaçāo []  *************/
+
+// código aqui
+
+
+
+
+
+/***********  # Alterar valores do objeto.  *************/
+
+// código aqui
+
+
+
+
+
+/***********  Remova o último item do array. *************/
+
+// código aqui
+
