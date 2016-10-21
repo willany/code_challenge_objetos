1c5697ed4bef861c2c48bf84d1edc2275c53436f
Willany Silva <willany_lp@hotmail.com>
2016-10-20 23:24:07 -0300

    code challenge objetos

--- objetos.js
+++ objetos.js
@@ -16,7 +16,7 @@
 /***********  # 1 Criar Objeto Notebook.*************/
 
 // código aqui
-
+notebook = {}
 
 
 
@@ -25,38 +25,47 @@
 /*********** # 2 Adicionando as propriedades. *************/
 
 // código aqui
-
-
-
-
+notebook.sistema = "Mac";
+notebook.screensize = 15;
+notebook.anoCompra = 2015;
 
 
 /***********  # Acessando objeto com a notaçāo "." *************/
 
 
 // código aqui
-
-
-
+console.log(notebook.sistema);
+console.log(notebook.screensize);
+console.log(notebook.anoCompra);
 
 
 /***********  Acessando objeto com a notaçāo []  *************/
 
 // código aqui
-
-
-
+console.log(notebook["sistema"]);
+console.log(notebook["screensize"]);
+console.log(notebook["anoCompra"]);
 
 
 /***********  # Alterar valores do objeto.  *************/
 
 // código aqui
+notebook.sistema = "Windows";
+notebook.screensize = 14;
+notebook.anoCompra = 2014;
 
 
+console.log(notebook);
 
 
 
 /***********  Remova o último item do array. *************/
 
 // código aqui
+var girls = ["Karina", "Simone", "Veridiana", "Willany"];
 
+console.log(girls);
+
+girls.pop();
+
+console.log(girls);
